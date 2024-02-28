3898c8d026d342ea5f9473944c0f5c075ccba441
Antonio Melé <antonio.mele@gmail.com>
2024-02-28 12:00:14 +0100

    fix

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from catalog.models import Track
+from catalog.models import Genre, Track
+
+
+@admin.register(Genre)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'created', 'updated']
+    search_fields = ['code', 'name']
+    list_filter = ['created', 'updated']
 
 
 @admin.register(Track)
 class TrackAdmin(admin.ModelAdmin):
     list_display = ['isrc', 'name', 'artist', 'duration', 'released', 'is_cover', 
                     'is_remix', 'is_instrumental', 'is_explicit', 'created', 'updated']
-    list_filter = ['released', 'is_remix', 'is_instrumental']
+    list_filter = ['released', 'is_remix', 'is_cover', 'is_instrumental', 'created', 'updated']
     search_fields = ['name', 'duration', 'artist__name']
     raw_id_fields = ['artist']

c904a002300accb2d9ba1b4b16bfa3a4f0858015
Antonio Melé <antonio.mele@gmail.com>
2024-05-10 16:36:16 +0200

    added celery

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn acrylic.wsgi
+worker: celery worker --app=tasks.app
--- acrylic/celery.py
+++ acrylic/celery.py
@@ -5,4 +5,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acrylic.settings')
 
 app = Celery('acrylic')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(
+    BROKER_URL=os.environ['REDISCLOUD_URL'],
+    CELERY_RESULT_BACKEND=os.environ['REDISCLOUD_URL']
+)
+
 app.autodiscover_tasks()

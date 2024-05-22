7b2fec4839aa1c909ec0c0a6e8b84a4670d82f65
Antonio Melé <antonio.mele@gmail.com>
2024-05-22 22:45:55 +0200

    fix price in track

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -34,7 +34,7 @@ class MyTrackSerializer(serializers.ModelSerializer):
     genres = GenreSerializer(many=True, required=False)
     additional_main_artists = serializers.SlugRelatedField(slug_field='uuid', read_only=True, many=True)
     featured_artists = serializers.SlugRelatedField(slug_field='uuid', read_only=True, many=True)
-    price = serializers.SlugRelatedField(slug_field='uuid', many=False)
+    price = serializers.SlugRelatedField(slug_field='uuid', queryset=Price.objects.all(), many=False)
 
     class Meta:
         model = Track

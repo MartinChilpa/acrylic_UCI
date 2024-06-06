6484b6bc9f18b4607d4f800994e64d6ff5bcb7c2
Antonio Melé <antonio.mele@gmail.com>
2024-06-06 12:12:00 +0200

    legal webhook fix

--- legal/webhooks.py
+++ legal/webhooks.py
@@ -9,38 +9,9 @@ from legal.signwell import Signwell
 
 @csrf_exempt
 @require_POST
-def hellosign_webhook(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        event = data.get('event')
-        
-        signed_date = None
-        if event == 'signature_request_signed':
-            signature_request_id = data.get('signature_request_id')
-            signatures = data['signature_request']['signatures']
-            for signature in signatures:
-                # The signed_date might be in UNIX timestamp format
-                signed_timestamp = signature.get('signed_at')
-                if signed_timestamp:
-                    # Convert the UNIX timestamp to a datetime object
-                    signed_date = datetime.utcfromtimestamp(int(signed_timestamp))
-                    print(f"Document was signed on: {signed_date}")
-
-        if signed_date:
-            # Here you would update the MasterSplit instance with the datetime of signing
-            # This is a simplified example. You will need to map the signature_request_id to your MasterSplit instance appropriately.
-            SplitSheet.objects.filter(signature_request_id=signature_request_id).update(signed=signed_date, status=SplitSheet.Status.SIGNED)
-        return JsonResponse({'status': 'success'})
-    return JsonResponse({'status': 'bad request'}, status=400)
-
-
-
-@csrf_exempt
-@require_POST
 def signwell_webhook(request):
     if request.method == 'POST':
-        data = request.POST
-        
+        data = json.loads(request.body)
         event = data.get('event')
         
         sign_backend = Signwell()
@@ -65,3 +36,30 @@ def signwell_webhook(request):
                 SplitSheet.objects.filter(signature_request_id=signature_request_id).update(signed=signed_date, status=SplitSheet.Status.SIGNED)
             return JsonResponse({'status': 'success'})
         return JsonResponse({'status': 'bad request'}, status=400)
+
+
+@csrf_exempt
+@require_POST
+def hellosign_webhook(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        event = data.get('event')
+        
+        signed_date = None
+        if event == 'signature_request_signed':
+            signature_request_id = data.get('signature_request_id')
+            signatures = data['signature_request']['signatures']
+            for signature in signatures:
+                # The signed_date might be in UNIX timestamp format
+                signed_timestamp = signature.get('signed_at')
+                if signed_timestamp:
+                    # Convert the UNIX timestamp to a datetime object
+                    signed_date = datetime.utcfromtimestamp(int(signed_timestamp))
+                    print(f"Document was signed on: {signed_date}")
+
+        if signed_date:
+            # Here you would update the MasterSplit instance with the datetime of signing
+            # This is a simplified example. You will need to map the signature_request_id to your MasterSplit instance appropriately.
+            SplitSheet.objects.filter(signature_request_id=signature_request_id).update(signed=signed_date, status=SplitSheet.Status.SIGNED)
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'bad request'}, status=400)

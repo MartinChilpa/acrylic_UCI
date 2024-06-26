468d9dc7c821d84158be6ea565c381fe2e4d5f63
Antonio Melé <antonio.mele@gmail.com>
2024-06-26 18:07:21 +0200

    add email pre-filled for invites

--- account/tasks.py
+++ account/tasks.py
@@ -10,7 +10,7 @@ def send_registration_invite(email, language):
         message = f"""
         Welcome to the Acrylic platform!
         
-        REGISTER HERE! {signup_url}
+        REGISTER HERE! {signup_url}?email={email}
         
         If you have any questions, just join us on Skool (https://www.skool.com/the-acrylic-kamunity/about), a free community where you can learn about your music rights and ask any questions you have about Acrylic or how it works.​
         
@@ -22,7 +22,7 @@ def send_registration_invite(email, language):
         message = f"""
         ¡Bienvenido a la plataforma Acrylic!
         
-        RÉGISTRATE AQUÍ {signup_url}
+        RÉGISTRATE AQUÍ {signup_url}?email={email}
 
         Si tienes alguna pregunta, únete a nosotros en Skool (https://www.skool.com/la-kamunidad-acrylic-8907/about), una comunidad gratuita donde puedes aprender sobre tus derechos musicales y hacer cualquier pregunta que tengas sobre Acrylic o cómo funciona.
     
